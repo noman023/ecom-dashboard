e8378bb9df50600b482fe6e9c96778b02a2b1a65
Noman <molla.noman23@gmail.com>
2025-06-28 13:56:46 +0600

    remove models for filename issue

--- client/src/utils/baseURL.js
+++ client/src/utils/baseURL.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:5000/api";
-// export const baseURL = "https://ecom-server-sand.vercel.app/api";
+// export const baseURL = "http://localhost:5000/api";
+export const baseURL = "https://ecom-server-sand.vercel.app/api";
